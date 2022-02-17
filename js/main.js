fdba6458d376d260bd69c2e49f0dcda5cbc6c139
Ilya <53115332+kuchinil@users.noreply.github.com>
2022-02-17 23:29:41 +0300

    сделан адаптив, гамбургер меню, мелкие правки

--- js/main.js
+++ js/main.js
@@ -3,6 +3,20 @@ const swiper = new Swiper('.main-slider', {
   spaceBetween: 30,
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
+    nextEl: '.main-slider-arrow',
   },
 })
+
+// Burger-menu
+let menuButtonClose = document.querySelector('.menu-button-close')
+let btnMenu = document.getElementsByClassName('btn-menu')
+let body = document.querySelector('.menu-opened')
+for (let i = 0; i < btnMenu.length; i++) {
+  btnMenu[i].addEventListener('click', function () {
+    body.classList.toggle('open-menu')
+  })
+}
+
+function closeMenu() {
+  body.classList.remove('open-menu')
+}
